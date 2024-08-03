f298d2fef562bc28237b231d8374c1c4b7a82b40
aditya-ranjan <ranjanaditya263@gmail.com>
2024-08-04 00:54:17 +0530

    Profile Page UI Design Completed Nested Scroll functionality Added on Profile

--- lib/views/profile/profile.dart
+++ lib/views/profile/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:threads_clone_app/utils/styles/button_styles.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -12,11 +14,138 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Profile"),
+        title: const Icon(Icons.language),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.sort),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text("I am Profile"),
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 160,
+                collapsedHeight: 160,
+                automaticallyImplyLeading: false,
+                flexibleSpace: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Aditya",
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                width: context.width * 0.7,
+                                child: const Text(
+                                  "Let's build threads clone in flutter.",
+                                ),
+                              ),
+                            ],
+                          ),
+                          const CircleAvatar(
+                            radius: 40,
+                            backgroundImage:
+                                AssetImage("assets/images/avatar.png"),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: OutlinedButton(
+                              onPressed: () {},
+                              style: customOutlineStyle(),
+                              child: const Text("Edit Profile"),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Expanded(
+                            child: OutlinedButton(
+                              onPressed: () {},
+                              style: customOutlineStyle(),
+                              child: const Text("Share Profile"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SliverPersistentHeader(
+                floating: true,
+                pinned: true,
+                delegate: SliverAppBarDelegate(
+                  const TabBar(
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    tabs: [
+                      Tab(
+                        text: "Threads",
+                      ),
+                      Tab(
+                        text: "Replies",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: const TabBarView(
+            children: [
+              Text("I am Threads"),
+              Text("I am Replies"),
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+// * Sliver persistentHeader class
+class SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar _tabBar;
+
+  SliverAppBarDelegate(this._tabBar);
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.black,
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
