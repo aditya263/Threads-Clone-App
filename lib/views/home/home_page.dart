e587f83216b4b14af42c39122a8d57afb5d1cb34
aditya-ranjan <ranjanaditya263@gmail.com>
2024-08-10 01:28:57 +0530

    Added pull to refresh on homepage

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -14,35 +14,38 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              title: Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Image.asset(
-                  "assets/images/logo.png",
-                  width: 40,
-                  height: 40,
+        child: RefreshIndicator(
+          onRefresh: () => controller.fetchThreads(),
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                title: Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    width: 40,
+                    height: 40,
+                  ),
+                ),
+                centerTitle: true,
+              ),
+              SliverToBoxAdapter(
+                child: Obx(
+                  () => controller.loading.value
+                      ? const Loading()
+                      : ListView.builder(
+                          shrinkWrap: true,
+                          padding: EdgeInsets.zero,
+                          physics: const BouncingScrollPhysics(),
+                          itemCount: controller.posts.length,
+                          itemBuilder: (context, index) => PostCard(
+                            post: controller.posts[index],
+                          ),
+                        ),
                 ),
               ),
-              centerTitle: true,
-            ),
-            SliverToBoxAdapter(
-              child: Obx(
-                () => controller.loading.value
-                    ? const Loading()
-                    : ListView.builder(
-                        shrinkWrap: true,
-                        padding: EdgeInsets.zero,
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: controller.posts.length,
-                        itemBuilder: (context, index) => PostCard(
-                          post: controller.posts[index],
-                        ),
-                      ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
